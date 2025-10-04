38038bede4b90a47bfed89bb55df0624e678d778
Alexandru Fulga <alexandrufulga13@gmail.com>
2025-10-04 22:37:12 +0200

    Añadir DTO para categoria y creación de categoria.

---
+++ ApiEcommerce/Controllers/CategoriesController.cs
@@ -0,0 +1,14 @@
+using ApiEcommerce.Repository.IRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+    }
+}
---
+++ ApiEcommerce/Models/Dtos/CreateCategoryDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiEcommerce.Models.Dtos;
+
+public class CreateCategoryDto
+{
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [MaxLength(50, ErrorMessage = "El nombre debe tener menos de 50 caracteres")]
+    [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres")]
+    public string Name { get; set; } = string.Empty;
+}
