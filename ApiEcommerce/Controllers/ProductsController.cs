07d8a913f4fc99fdc7b3a3978c1b1d237bc7b2a4
Alexandru Fulga <alexandrufulga13@gmail.com>
2025-10-12 12:23:14 +0200

    Añadir controlador para productos.

---
+++ ApiEcommerce/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+using ApiEcommerce.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+
+    }
+}
